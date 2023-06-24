46448b85669266fa24594e6de653a4c318606c93
Xerlok <20791727+Xerlok@users.noreply.github.com>
2023-06-24 22:45:42 +0300

    Make remove and read buttons work

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let forma = document.getElementById('forma');
 let userInput = document.getElementById('form-container');
 let bookshelf = document.getElementById('shelf');
 let check = document.getElementById('read');
+let formBtn = document.querySelector('.newBook');
 
 let myLibrary = [];
 
@@ -17,21 +18,22 @@ function createBook (title, author, pages, read) {
     myLibrary.push(newBook);
 }
 
-{/* <div class="author bookText">J.R.R. Tolkien</div>
-<div class="pages bookText">333 pages</div> */}
-
 function addBook() {
-    let book = document.createElement('div');
-    let bookTitle = document.createElement('div');
-    let bookAuthor = document.createElement('div');
-    let bookPages = document.createElement('div');
-    let removeBtn = document.createElement('button');
-    let checkbox = document.createElement('input');
-    let checkLabel = document.createElement('label');
+    while(bookshelf.hasChildNodes()){
+    bookshelf.removeChild(bookshelf.firstChild);
+    }
 
-    for (i=0; i<myLibrary.length; i++) {
-        console.log(myLibrary[i].read);
+    for (i = 0; i < myLibrary.length; i++) {
+        let book = document.createElement('div');
+        let bookTitle = document.createElement('div');
+        let bookAuthor = document.createElement('div');
+        let bookPages = document.createElement('div');
+        let removeBtn = document.createElement('button');
+        let checkbox = document.createElement('input');
+        let checkLabel = document.createElement('label');
+
         book.className = 'book';
+        book.dataset.titleName = myLibrary[i].title;
         bookTitle.className = 'bookTitle bookText';
         bookTitle.innerText = myLibrary[i].title;
         bookAuthor.className = 'author bookText'
@@ -40,12 +42,19 @@ function addBook() {
         bookPages.innerText = myLibrary[i].pages;
         removeBtn.className = 'remove';
         removeBtn.innerText = 'Remove';
-        removeBtn.addEventListener('click', function(){
+        removeBtn.addEventListener('click', function() {
             this.parentElement.remove();
+
+            const titleToSplice = myLibrary.findIndex(el => el.title === book.dataset.titleName);
+            myLibrary.splice(titleToSplice, 1);
         });
         checkbox.type = 'checkbox';
         checkbox.id = 'readBox';
         checkbox.checked = myLibrary[i].read;
+        checkbox.addEventListener('click', function() {
+            const index = myLibrary.findIndex(el => el.title === book.dataset.titleName);
+            myLibrary[index].read = checkbox.checked;
+        })
         checkLabel.innerText = 'Read';
         checkLabel.setAttribute('for', read);
         
@@ -54,14 +63,6 @@ function addBook() {
     }
 
 
-}
-
-function openForm() {
-    forma.style.display = "block";
-}
-  
-function closeForm() {
-    forma.style.display = "none";
 }
 
 userInput.addEventListener ('submit', (e)=> {
@@ -76,3 +77,13 @@ userInput.addEventListener ('submit', (e)=> {
 
     userInput.reset();
 })
+
+function openForm() {
+    forma.style.display = "block";
+    formBtn.style.setProperty('box-shadow', 'none');
+}
+  
+function closeForm() {
+    forma.style.display = "none";
+    formBtn.style.setProperty('box-shadow', '0px 5px 5px rgba(0, 0, 0, 0.555)');
+}
